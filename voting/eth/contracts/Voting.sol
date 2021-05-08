c9bbffeaa1b65b447de7ed149452766a6de29c0a
Jamie Rumbelow <jamie@jamierumbelow.net>
2021-05-08 11:36:07 +0100

    Voting: restructure Election struct, create new election
    we can't nest mappings inside structs inside mappings (I think because
    mappings are only instantiatable inside the contract's storage, not its
    working memory). so we need to bring the votes mapping up into storage.
    
    similar story with candidates, except candidates array needs to sit in
    storage since its length is unknown at instantiation

--- voting/eth/contracts/Voting.sol
+++ voting/eth/contracts/Voting.sol
@@ -2,17 +2,94 @@
 pragma solidity ^0.8.4;
 
 contract Voting {
+    // State
+    // -------------------------------------------------------------------------
+
+    struct Candidate {
+        address owner;
+        string name;
+    }
+
     struct Election {
+        // - metadata
+        string title;
         // - start / end times
         uint256 beginRegistrationAtBlock;
         uint256 endRegistrationAtBlock;
         uint256 beginVotingAtBlock;
         uint256 endVotingAtBlock;
-        // - voting mechanics
-        address[] candidates;
-        mapping(address => address) votes;
     }
 
-    // - owner address => Election
-    mapping(address => Election) public elections;
+    uint256 internal electionIdCounter = 0;
+    mapping(address => uint256[]) public ownerElectionIds;
+    mapping(uint256 => Election) public elections;
+    mapping(uint256 => Candidate[]) public candidates;
+    // electionId => [ voterAddress => candidateAddress ]
+    mapping(uint256 => mapping(address => address)) public votes;
+
+    // Public Functions
+    // -------------------------------------------------------------------------
+
+    function createElection(
+        string memory _title,
+        uint256 _beginRegistrationAtBlock,
+        uint256 _endRegistrationAtBlock,
+        uint256 _beginVotingAtBlock,
+        uint256 _endVotingAtBlock
+    ) public returns (uint256) {
+        require(bytes(_title).length > 0, "title must be non-empty");
+        require(
+            _beginRegistrationAtBlock > block.timestamp,
+            "registration must begin in the future"
+        );
+        require(
+            _endRegistrationAtBlock > _beginRegistrationAtBlock,
+            "registration must end after it begins"
+        );
+        require(
+            _beginVotingAtBlock > _endRegistrationAtBlock,
+            "voting must begin after registration ends"
+        );
+        require(
+            _endVotingAtBlock > _beginVotingAtBlock,
+            "voting must end after it begins"
+        );
+
+        return
+            _createElection(
+                msg.sender,
+                _title,
+                _beginRegistrationAtBlock,
+                _endRegistrationAtBlock,
+                _beginVotingAtBlock,
+                _endVotingAtBlock
+            );
+    }
+
+    // Private Functions
+    // -------------------------------------------------------------------------
+
+    function _createElection(
+        address _owner,
+        string memory _title,
+        uint256 _beginRegistrationAtBlock,
+        uint256 _endRegistrationAtBlock,
+        uint256 _beginVotingAtBlock,
+        uint256 _endVotingAtBlock
+    ) internal returns (uint256) {
+        uint256 electionId = _nextElectionId();
+        ownerElectionIds[_owner].push(electionId);
+        elections[electionId] = Election(
+            _title,
+            _beginRegistrationAtBlock,
+            _endRegistrationAtBlock,
+            _beginVotingAtBlock,
+            _endVotingAtBlock
+        );
+        return electionId;
+    }
+
+    function _nextElectionId() internal returns (uint256) {
+        return electionIdCounter++;
+    }
 }
